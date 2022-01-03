c4613891c5b68fe74c0c6a84d4525d3f67403bf9
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-01-03 11:36:23 -0600

    String Basics excercise 1

---
+++ src/StringExercise.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//For each of the following output examples: create a String variable
+// that contains the desired output and print it out to the console,
+// you can do this with only one String variable and one print statement
+// for each output example.
+
+        String noEducation = "We don't need no education\nWe don't need no thought control";
+        System.out.println(noEducation);
+
+        String check = "Check \"this\" out!, \"s inside of \"s!\"";
+        System.out.println(check);
+
+        String windowsDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(windowsDrive);
+
+        String backslashes ="I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+
+
+
+
+
+
+
+
+    }
+
+}

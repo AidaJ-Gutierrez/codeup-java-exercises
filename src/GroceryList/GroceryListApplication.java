0b0e304e06552b71e8685bd6afe78b0062adecdb
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-01-20 15:45:54 -0600

    some updates

--- src/GroceryList/GroceryListApplication.java
+++ src/GroceryList/GroceryListApplication.java
@@ -1,15 +1,28 @@
 package GroceryList;
-
-import util.Input;
-import java.util.HashMap;
-//public class GroceryListApplication {
+//public class GroceryApplication {
+//
+//    //prompts user for item
+//    public static void promptUserForItem(HashMap<Item, String> groceryList){
+//        boolean continuePrompting = true;
+//        while(continuePrompting){
+//            addToGroceryList(groceryList);
+//            continuePrompting = askUserToContinue();
+//        }
+//        displayGroceryList();
+//    }
+//
+//
+//    //create new Item on the grocery list with user input
+//    public static void addToGroceryList(HashMap<Item, String> groceryList){
+//        Input input = new Input();
+//        System.out.println("Enter an Item: ");
+//        String userItem = input.getString();
+//        System.out.println("Enter Quantity of Item: ");
+//        int userItemQuantity = input.getInt();
 //
 //
 //
 //
 //
-//
-//    public static void main(String[] args){
 //
 //    }
-//}
--- src/GroceryList/item.java
+++ src/GroceryList/item.java
@@ -1,20 +1,20 @@
 package GroceryList;
-//
 
-//public class item {
-//    private String itemName;
-//    private int itemQuantity;
-//
-//    public Item(String itemName, int itemQuantity){
-//        this.itemName = itemName;
-//        this.itemQuantity = itemQuantity;
-//    }
-//
-//    public String getItemName(){
-//        return itemName;
-//    }
-//
-//    public int getItemQuantity(){
-//        return itemQuantity;
-//    }
-//}
+
+public class item {
+    private String itemName;
+    private int itemQuantity;
+
+    public void Item(String itemName, int itemQuantity){
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public int getItemQuantity(){
+        return itemQuantity;
+    }
+}
